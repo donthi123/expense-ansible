af07846ffd188dac66ca01164d0d8e10d4acad3e
derangulasreevidhya <derangulasreevidhya@gmail.com>
2024-07-15 16:24:34 -0500

    create all pipelines

--- pipelines/ansible.sh
+++ pipelines/ansible.sh
@@ -1,3 +1,3 @@
 component=$1
 env
-ansible-playbook -i $component-dev.donthi123.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev -e role_name=$component expense.yml
+ansible-playbook -i $component-$env.donthi123.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=$env -e role_name=$component expense.yml
