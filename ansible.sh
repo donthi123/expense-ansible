7a25beefbd2b085010f87889ea02fa1915b9789b
derangulasreevidhya <derangulasreevidhya@gmail.com>
2024-07-29 12:58:07 -0500

    testing secure variables

--- ansible.sh
+++ ansible.sh
@@ -2,3 +2,4 @@ component=$1
 env >/tmp/env
 
 #ansible-playbook -i $component-dev.donthi123.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev -e role_name=$component expense.yml
+#
