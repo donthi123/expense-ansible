e5e65b70bea9cdf5782f06dd816ecf80f2a13e35
derangulasreevidhya <derangulasreevidhya@gmail.com>
2024-07-31 11:26:36 -0500

    update secret path

--- ansible.sh
+++ ansible.sh
@@ -2,4 +2,5 @@ component=$1
 
 ansible-playbook get.secrets.yml -e vault_token=$vault_token -e env=$env -e role_name=$component
 
-ansible-playbook -i $component-$env.donthi123.online, -e env=$env -e role_name=$component expense.yml -e '@secrets.json' -e '@app.json'
+ansible-playbook -i $component-$env.donthi123.online, -e env=$env -e role_name=$component expense.yml -e '@~/secrets.json' -e '@~/app.json'
+"rm -f ~/secrets.json ~/app.json"
