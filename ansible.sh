72c8f9d5d6d613f255431ad08713363b4882b3c4
derangulasreevidhya <derangulasreevidhya@gmail.com>
2024-08-17 16:36:51 -0500

    trying single secret approach

--- ansible.sh
+++ ansible.sh
@@ -1,6 +1,7 @@
 component=$1
-
+rm -f ~/*.json
 ansible-playbook get.secrets.yml -e vault_token=$vault_token -e env=$env -e role_name=$component
+exit 1
 
-ansible-playbook -i $component-$env.donthi123.online, -e env=$env -e role_name=$component expense.yml -e '@~/secrets.json' -e '@~/app.json'
-rm -f ~/secrets.json ~/app.json
+ansible-playbook -i $component-$env.donthi123.online, -e env=$env -e role_name=$component expense.yml -e '@~/secrets.json'
+rm -f ~/*.json
